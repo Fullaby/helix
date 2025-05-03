ff375680bd9c2a39f48e72f0473313db65fca807
Steven Leonardo <Stevenleonardo57@gmail.com>
2025-05-04 06:44:33 +0700

    fix

--- src/components/Marketingbar.jsx
+++ src/components/Marketingbar.jsx
@@ -1,7 +1,4 @@
 import { useNavigate, NavLink } from "react-router-dom";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 function Marketingbar() {
     const items = [
